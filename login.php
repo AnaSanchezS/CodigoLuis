4529f547cb6891ab6716e8467d03b862c70ab1f7
AnaSanchezS <33376532+AnaSanchezS@users.noreply.github.com>
2019-07-09 22:29:55 -0500

    update login

--- login.php
+++ login.php
@@ -7,6 +7,8 @@
 	
 	$this->pdo->query("CREATE TABLE users (usuario VARCHAR(50) NOT NULL, contrasena VARCHAR(200) NOT NULL)");
 	
+	public function contrasena($contrasena)
+    {
 	$usuario="Luis";
 	$contrasenaEncriptada=password_hash("Hola", PASSWORD_DEFAULT);
 	$queryIngresarUsuario="INSERT INTO `users`(`usuario`, `contrasena`) VALUES ('$usuario','$contrasenaEncriptada')";
@@ -19,14 +21,9 @@
 	$result = mysqli_query($conn, $query1);
 	if(mysqli_num_rows($result)!=0){
 	while($data= mysqli_fetch_assoc($result) ) {
-	// $IDUsuario = $data['ID'];
-	//$usuario = $data['nombre'];
+  
 	$contrasena = $data['contrasena'];
-	// $nombreUsuario=$data['nombre'];
-	//$apellidoUsuario=$data['apellido'];
-	// $urf = $data['urf'];
-	//$tipo_usuario = $data['tipo'];
-	// $psw=password_verify($psw, $contrasena);
+	
 	}
 	if(password_verify($psw, $contrasena)){
 	
@@ -35,7 +32,4 @@
 	}
 	
 	}
-	
-	
-	
-	?>
+	}
