ca0acb5506e7ceb2330d01e6f06a18d7e7f4633a
AnaSanchezS <33376532+AnaSanchezS@users.noreply.github.com>
2019-07-09 20:01:15 -0500

    HelloWorldTest.php

---
+++ Tests/HelloWorldTest.php
@@ -0,0 +1,34 @@
+<?php
+
+class HelloWorldTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function setUp()
+    {
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->query("CREATE TABLE login (user VARCHAR(50) NOT NULL)");
+    }
+
+	public function tearDown()
+    {
+        $this->pdo->query("DROP TABLE login");
+    }
+	
+     
+    public function testHelloWorld()
+    {
+        $helloWorld = new HelloWorld($this->pdo);
+
+        $this->assertEquals('contraseña', $contrasena->hello());
+    }
+
+    
+
+    
+}
+
